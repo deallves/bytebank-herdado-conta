92b9237480faabca2578fa69bfed3d02cf881980
deallves <deallves@gmail.com>
2022-08-10 21:42:19 -0300

    Finalização do curso, polimorfismo, herança.

--- src/Conta.java
+++ src/Conta.java
@@ -1,5 +1,6 @@
-public class Conta {
-	private double saldo;
+public  abstract class Conta {
+	
+    protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular;
@@ -16,10 +17,7 @@ public class Conta {
 		
 	}
 
-	public void deposita(double valor) {
-		this.saldo = this.saldo + valor;
-
-	}
+	public abstract void deposita(double valor); 
 
 	public boolean saca(double valor) {
 		if (this.saldo >= valor) {
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,5 +1,7 @@
+ 
 
-public class ContaCorrente extends Conta {
+
+public class ContaCorrente extends Conta implements Tributavel {
 	
 	
 	public ContaCorrente(int agencia, int numero) {
@@ -11,6 +13,17 @@ public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.2;
 		return super.saca(valorASacar);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo  += valor;
+		
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo *0.01;
 	}
 
 }
